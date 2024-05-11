18dc4c378ac8b159fdd4bf1f686807fe76ee7590
tilt-rgb <fake8926@gmail.com>
2024-05-11 17:01:53 +0000

    sdm660-common: drop lineage fast charge & remove packages

--- sdm660.mk
+++ sdm660.mk
@@ -413,10 +413,6 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # RIL
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
